65484be29fc7af3a79e43d82d562f7fc81c64daf
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-06-19 21:56:14 -0700

    Initial commit

---
+++ switches.js
@@ -0,0 +1,40 @@
+const appState = {
+  isLightOn: false,
+  switches: [{name: "A", state: "false"}],
+};
+
+const LightBulbComponent = () => {
+  if (appState.isLightOn) {
+    return `<img src="lightOn.png" width="230" height="300">`;
+  } else {
+    return `<img src="lightOff.png" width="200" height="300">`;
+  }
+};
+
+const switchesComponent = () => {
+  return `<table style="margin: auto;">
+    <tr>
+      ${appState.switches.map((switchObj) => `<th>${switchObj.name}</th>`).join("")}
+    </tr>
+    <tr>
+      ${appState.switches.map(switchComponent).join("")}
+    </tr>
+  </table>`;
+};
+
+const switchComponent = (switchObj, switchNumber) => {
+  const switchImage = switchObj.state
+    ? `<img src="switchOn.png" width="65" height="100">`
+    : `<img src="switchOff.png" width="65" height="100">`;
+  return `<td><div style="padding: 5">${switchImage}</div></td>`;
+};
+
+const App = () => {
+  const light = LightBulbComponent();
+  const switches = switchesComponent();
+  const app = `${light}${switches}`;
+  console.log(app);
+  return app;
+};
+
+document.getElementById("root").innerHTML = App();
